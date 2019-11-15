7d1c69fd497a9005ceb91fb00f2e847b743f8a1b
kevton <kevin.mathe.g@gmail.com>
2019-11-15 16:33:33 +0100

    Réponse

--- exercice1.php
+++ exercice1.php
@@ -9,15 +9,43 @@ class Diviseur {
     {
         $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
 
+        if ( $index > 9) {
+            throw new Erreur("Votre index doit être inferieur 9 ");
+        }
+        elseif ($index < 0){
+            throw new Erreur("Votre index doit être superieur à 0 ");
+        }
+        if ($diviseur === 0){
+            throw new Erreur("Votre diviseur doit être different de 0 ");
+
+        }
         return $valeurs[$index] / $diviseur;
     }
 }
+class Erreur extends \Exception{}
 
-$input = $climate->input("Entrez l’indice de l’entier à diviser : ");
-$index = $input->prompt();
+do{
+    try{
 
-$input = $climate->input("Entrez le diviseur : ");
-$diviseur = $input->prompt();
+        $input = $climate->input("Entrez l’indice de l’entier à diviser : ");
+        $index = $input->prompt();
 
-$climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+        $input = $climate->input("Entrez le diviseur : ");
+        $diviseur = $input->prompt();
 
+        $result =  (new Diviseur())->division($index, $diviseur);
+
+        $climate->output("Le résultat de la division est : " . $result);
+    }catch(Erreur $e)
+    {
+        echo $e->getMessage();
+    }
+    catch(\TypeError $e){
+        echo "Vous n'avez pas entré un entier ";
+    }
+    catch(\Throwable $e)
+    {
+        echo "Erreur inconnue : " . $e->getMessage();
+    }
+      
+}while(!isset($result));
